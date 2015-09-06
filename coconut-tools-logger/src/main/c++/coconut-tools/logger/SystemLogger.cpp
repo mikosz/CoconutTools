0747020df90dc56720fd6ac532fbbdbcc2be7951
Mikołaj Radwan <mikoszrrr@gmail.com>
2015-09-06 09:04:30 +0200

    Make the default system logger also log in the debug window on MSVC

--- coconut-tools-logger/src/main/c++/coconut-tools/logger/SystemLogger.cpp
+++ coconut-tools-logger/src/main/c++/coconut-tools/logger/SystemLogger.cpp
@@ -1,7 +1,10 @@
 #include "SystemLogger.hpp"
 
+#include "coconut-tools/system/platform.hpp"
+
 #include "layout/EmptyLayout.hpp"
 #include "appender/ConsoleAppender.hpp"
+#include "appender/DebugWindowAppender.hpp"
 
 using namespace coconut_tools;
 using namespace coconut_tools::logger;
@@ -11,4 +14,8 @@ SystemLogger::SystemLogger() :
 {
 	layout::LayoutSharedPtr layout(new layout::EmptyLayout());
 	addAppender(std::make_shared<appender::ConsoleAppender>(Level::TRACE, layout));
+
+#if defined(CT_COMPILER_VISUAL_CXX)
+	addAppender(std::make_shared<appender::DebugWindowAppender>(Level::TRACE, layout));
+#endif /* CT_COMPILER_VISUAL_CXX */
 }
