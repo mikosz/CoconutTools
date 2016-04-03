ae7084f0c15a2e93a8f709d4faeb89dd90c037e4
Mikołaj Radwan <mikoszrrr@gmail.com>
2016-04-03 15:09:17 +0200

    Working enum tool

--- coconut-tools-enum/src/main/c++/coconut-tools/enum.hpp
+++ coconut-tools-enum/src/main/c++/coconut-tools/enum.hpp
@@ -3,16 +3,25 @@
 
 #include <boost/preprocessor.hpp>
 
+#include <iosfwd>
 #include <unordered_map>
 
 /**
- * Usage example:
+ * Usage example with incremental values starting at 0:
  *
  * CCN_ENUM(
+ *     Colour,
+ *     (BLACK)
+ *     (RED)
+ *     (BLUE)
+ * )
+ *
+ * with custom values:
+ *
+ * CCN_ENUM_VALUES(
  *     Shape,
- *     (TRIANGLE)
- *     (SQUARE)
- *     (HEXAGON)
+ *     (TRIANGLE)(3)
+ *     (SQUARE)(4)
  * )
  *
  * Creates an enum class with TRIANGLE, SQUARE and HEXAGON with specified values and two functions:
@@ -29,25 +38,82 @@
 #define CCN_ENUM_nameValue(r, data, value) \
 	{ BOOST_PP_STRINGIZE(value) BOOST_PP_COMMA() data ## ::value } BOOST_PP_COMMA()
 
-#define CCN_ENUM(EnumName, values) \
+#define CCN_ENUM_is_column_left(i) \
+	BOOST_PP_EQUAL(BOOST_PP_MOD(i, 2), 0)
+
+#define CCN_ENUM_eq(value) \
+	BOOST_PP_CAT(= value, BOOST_PP_COMMA())
+
+#define CCN_ENUM_valueName_incrementing(EnumName, values) \
+	BOOST_PP_SEQ_FOR_EACH(CCN_ENUM_valueName, EnumName, values)
+
+#define CCN_ENUM_valueName_provided_i(r, data, i, value) \
+	BOOST_PP_IF( \
+		CCN_ENUM_is_column_left(i), \
+		CCN_ENUM_valueName, \
+		BOOST_PP_TUPLE_EAT(3) \
+		)(r, data, value)
+
+#define CCN_ENUM_valueName_provided(EnumName, values) \
+	BOOST_PP_SEQ_FOR_EACH_I(CCN_ENUM_valueName_provided_i, EnumName, values)
+
+#define CCN_ENUM_nameValue_incrementing(EnumName, values) \
+	BOOST_PP_SEQ_FOR_EACH(CCN_ENUM_nameValue, EnumName, values)
+
+#define CCN_ENUM_nameValue_provided_i(r, data, i, value) \
+	BOOST_PP_IF( \
+		CCN_ENUM_is_column_left(i), \
+		CCN_ENUM_nameValue, \
+		BOOST_PP_TUPLE_EAT(3) \
+		)(r, data, value)
+
+#define CCN_ENUM_nameValue_provided(EnumName, values) \
+	BOOST_PP_SEQ_FOR_EACH_I(CCN_ENUM_nameValue_provided_i, EnumName, values)
+
+#define CCN_ENUM_value_eq(r, data, i, value) \
+	BOOST_PP_IF( \
+		CCN_ENUM_is_column_left(i), \
+		value BOOST_PP_TUPLE_EAT(1), \
+		CCN_ENUM_eq \
+		)(value)
+
+#define CCN_ENUM_domain_incrementing(values) \
+	BOOST_PP_SEQ_FOR_EACH(CCN_ENUM_value, 0, values)
+
+#define CCN_ENUM_domain_provided(values) \
+	BOOST_PP_SEQ_FOR_EACH_I(CCN_ENUM_value_eq, 0, values)
+
+#define CCN_ENUM_definitions(EnumName, values, domainType) \
 	enum class EnumName { \
-		BOOST_PP_SEQ_FOR_EACH(CCN_ENUM_value, 0, values) \
+		BOOST_PP_CAT(CCN_ENUM_domain_, domainType)(values) \
 	}; \
 	\
 	\
 	inline const std::string& toString(EnumName value) { \
 		/* TODO: this builds on visual c++, but is probably invalid C++ (no hash function for enum class) */ \
 		static const std::unordered_map<EnumName, std::string> EnumName ## _NAMES = { \
-			BOOST_PP_SEQ_FOR_EACH(CCN_ENUM_valueName, EnumName, values) \
+			BOOST_PP_CAT(CCN_ENUM_valueName_, domainType)(EnumName, values) \
 			}; \
 		return EnumName ## _NAMES.at(value); \
 	} \
 	\
 	void fromString(EnumName& value, const std::string& name) { \
 		static const std::unordered_map<std::string, EnumName> NAMES_TO_ ## EnumName = { \
-			BOOST_PP_SEQ_FOR_EACH(CCN_ENUM_nameValue, EnumName, values) \
+			BOOST_PP_CAT(CCN_ENUM_nameValue_, domainType)(EnumName, values) \
 			}; \
+		/* TODO: this will throw out_of_range if name not found. return optional or throw custom exception */ \
 		value = NAMES_TO_ ## EnumName.at(name); \
+	} \
+	\
+	inline std::ostream& operator<<(std::ostream& os, EnumName value) { \
+		os << toString(value); \
+		return os; \
 	}
+
+#define CCN_ENUM(EnumName, values) \
+	CCN_ENUM_definitions(EnumName, values, incrementing)
+
+#define CCN_ENUM_VALUES(EnumName, values) \
+	CCN_ENUM_definitions(EnumName, values, provided)
 
 #endif /* COCONUTTOOLS_ENUM_HPP_ */
--- coconut-tools-enum/src/test/c++/coconut-tools/enum.cpp
+++ coconut-tools-enum/src/test/c++/coconut-tools/enum.cpp
@@ -10,26 +10,55 @@ CCN_ENUM(TestEnum,
 	(VALUE2)
 	);
 
+CCN_ENUM_VALUES(TestEnumValues,
+	(TRIANGLE)(3)
+	(SQUARE)(4)
+	);
+
 BOOST_AUTO_TEST_SUITE(EnumTestSuite);
 
-BOOST_AUTO_TEST_CASE(EnumValuesAreIncrementingIntegers) {
+BOOST_AUTO_TEST_CASE(DefaultEnumValuesAreIncrementingIntegers) {
 	BOOST_CHECK_EQUAL(static_cast<int>(TestEnum::VALUE0), 0);
 	BOOST_CHECK_EQUAL(static_cast<int>(TestEnum::VALUE1), 1);
 	BOOST_CHECK_EQUAL(static_cast<int>(TestEnum::VALUE2), 2);
+}
+
+BOOST_AUTO_TEST_CASE(ProvidedEnumValuesAreSet) {
+	BOOST_CHECK_EQUAL(static_cast<int>(TestEnumValues::TRIANGLE), 3);
+	BOOST_CHECK_EQUAL(static_cast<int>(TestEnumValues::SQUARE), 4);
 }
 
 BOOST_AUTO_TEST_CASE(EnumToStringYieldsName) {
 	BOOST_CHECK_EQUAL(toString(TestEnum::VALUE0), "VALUE0");
 	BOOST_CHECK_EQUAL(toString(TestEnum::VALUE1), "VALUE1");
 	BOOST_CHECK_EQUAL(toString(TestEnum::VALUE2), "VALUE2");
+	BOOST_CHECK_EQUAL(toString(TestEnumValues::TRIANGLE), "TRIANGLE");
+	BOOST_CHECK_EQUAL(toString(TestEnumValues::SQUARE), "SQUARE");
 }
 
 BOOST_AUTO_TEST_CASE(EnumFromStringReadsName) {
-	BOOST_FAIL("Unimplemented");
+	{
+		TestEnum value;
+		fromString(value, "VALUE0");
+		BOOST_CHECK_EQUAL(value, TestEnum::VALUE0);
+		fromString(value, "VALUE1");
+		BOOST_CHECK_EQUAL(value, TestEnum::VALUE1);
+		fromString(value, "VALUE2");
+		BOOST_CHECK_EQUAL(value, TestEnum::VALUE2);
+	}
+
+	{
+		TestEnumValues value;
+		fromString(value, "TRIANGLE");
+		BOOST_CHECK_EQUAL(value, TestEnumValues::TRIANGLE);
+		fromString(value, "SQUARE");
+		BOOST_CHECK_EQUAL(value, TestEnumValues::SQUARE);
+	}
 }
 
 BOOST_AUTO_TEST_CASE(EnumFromStringThrowsOnBadName) {
-	BOOST_FAIL("Unimplemented");
+	TestEnum value;
+	BOOST_CHECK_THROW(fromString(value, "NOT_A_VALUE_STRING"), std::out_of_range); // TODO: use a custom exception
 }
 
 BOOST_AUTO_TEST_SUITE_END(/* EnumTestSuite */);
