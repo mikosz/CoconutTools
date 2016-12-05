e12458611f449f860684ee2e7bf0a529c43b6742
Mikolaj Radwan <mikoszrrr@gmail.com>
2016-12-06 00:22:36 +0100

    Workaround for GMock boost::filesystem::path bug

--- coconut-tools-test-utils/src/main/c++/coconut-tools/test-utils/GMockFixture.hpp
+++ coconut-tools-test-utils/src/main/c++/coconut-tools/test-utils/GMockFixture.hpp
@@ -1,6 +1,9 @@
 #ifndef COCONUT_TOOLS_TEST_UTILS_GMOCKFIXTURE_HPP_
 #define COCONUT_TOOLS_TEST_UTILS_GMOCKFIXTURE_HPP_
 
+#include <iosfwd>
+
+#include <boost/filesystem/path.hpp>
 #include <boost/test/unit_test.hpp>
 
 #include <gmock/gmock.h>
@@ -46,5 +49,17 @@ public:
 
 } // namespace test_utils
 } // namespace coconut_tools
+
+// Wokaround for GMocks error that caused a stack overflow when mocking functions that accepted
+// boost::filesystem::path parameters
+namespace boost {
+namespace filesystem {
+
+inline void PrintTo(const boost::filesystem::path& path, std::ostream* os) {
+    *os << path;
+}
+
+} // namespace filesystem
+} // namespace boost
 
 #endif /* COCONUT_TOOLS_TEST_UTILS_GMOCKFIXTURE_HPP_ */
