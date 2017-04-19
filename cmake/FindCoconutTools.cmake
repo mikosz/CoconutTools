0686220136c8fae828367e594edb1ec5fddf7b1c
Mikołaj Radwan <mikoszrrr@gmail.com>
2017-04-19 12:29:47 +0200

    Updated find module for CoconutTools

--- cmake/FindCoconutTools.cmake
+++ cmake/FindCoconutTools.cmake
@@ -4,8 +4,30 @@
 #  CoconutTools_INCLUDE_DIR, where to find CoconutTools.h, etc.
 #  CoconutTools_LIBRARY_DIR, the library dir containing separate CoconutTools libs.
 #  CoconutTools_FOUND, If false, do not try to use CoconutTools.
-# also defined, but not for general use are
-#  CoconutTools_LIBRARY, where to find the CoconutTools library.
+#  CoconutTools_<C>_LIBRARY, where to find the CoconutTools <C> library, where <C> is a
+#    component name, such as LOGGER, SYSTEM etc
+
+macro(_FIND_COMPONENT COMPONENT)
+  find_library(
+    CoconutTools_${COMPONENT}_LIBRARY_DEBUG
+    "coconut-tools-${COMPONENT}.d"
+    HINTS ${CoconutTools_LIBRARY_HINT}
+    PATH_SUFFIXES lib
+    )
+
+  find_library(
+    CoconutTools_${COMPONENT}_LIBRARY_RELEASE
+    "coconut-tools-${COMPONENT}"
+    HINTS ${CoconutTools_LIBRARY_HINT}
+    PATH_SUFFIXES lib
+    )
+
+  set(
+    CoconutTools_${COMPONENT}_LIBRARY
+    debug ${CoconutTools_${COMPONENT}_LIBRARY_DEBUG}
+    optimized ${CoconutTools_${COMPONENT}_LIBRARY_RELEASE}
+    )
+endmacro(_FIND_COMPONENT)
 
 if(DEFINED CoconutTools_ROOT)
   set(CoconutTools_ALL_HINT ${CoconutTools_ROOT}/src)
@@ -20,24 +42,26 @@ find_path(
   PATH_SUFFIXES include
   )
 
-find_library(
-  CoconutTools_UTILS_LIBRARY
-  "coconut-tools-utils"
-  HINTS ${CoconutTools_LIBRARY_HINT}
-  PATH_SUFFIXES lib
-  )
-
-get_filename_component(
-  CoconutTools_LIBRARY_DIR
-  ${CoconutTools_UTILS_LIBRARY}
-  DIRECTORY
-  )
-
+_FIND_COMPONENT(CONFIGURATION)
+_FIND_COMPONENT(EXCEPTIONS)
+_FIND_COMPONENT(FACTORY)
+_FIND_COMPONENT(LOGGER)
+_FIND_COMPONENT(SERIALISATION)
+_FIND_COMPONENT(SYSTEM)
+_FIND_COMPONENT(UTILS)
+  
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
   CoconutTools
   DEFAULT_MSG
-  CoconutTools_LIBRARY_DIR CoconutTools_INCLUDE_DIR
+  CoconutTools_INCLUDE_DIR
+  CoconutTools_CONFIGURATION_LIBRARY
+  CoconutTools_EXCEPTIONS_LIBRARY
+  CoconutTools_FACTORY_LIBRARY
+  CoconutTools_LOGGER_LIBRARY
+  CoconutTools_SERIALISATION_LIBRARY
+  CoconutTools_SYSTEM_LIBRARY
+  CoconutTools_UTILS_LIBRARY
   )
 
 mark_as_advanced(CoconutTools_LIBRARY_DIR CoconutTools_INCLUDE_DIR)
